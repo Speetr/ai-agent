2b44dee832326da3c4054c29d33a64d481ea4ef1
Speetr <peetrsullivan@gmail.com>
2026-02-02 00:40:32 -0800

    L7: verbose output

--- main.py
+++ main.py
@@ -12,17 +12,21 @@ def main():
     
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     # Now we can access `args.user_prompt`
 
     client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     gemini_content = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+
     if gemini_content.usage_metadata == None:
         raise RuntimeError("Failed API request.")
     else:
-        print(f"Prompt tokens: {gemini_content.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {gemini_content.usage_metadata.candidates_token_count}")
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {gemini_content.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {gemini_content.usage_metadata.candidates_token_count}")
     print(f"Response: {gemini_content.text}")
 
 
