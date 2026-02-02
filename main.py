2bf4f6698ca9c71f55c28afb5a096c90330cf06d
Speetr <peetrsullivan@gmail.com>
2026-02-02 00:29:28 -0800

    L5: added user prompt as cli argument

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
         raise RuntimeError("API key not found.")
+    
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
 
     client = genai.Client(api_key=api_key)
-    gemini_content = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    gemini_content = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
     if gemini_content.usage_metadata == None:
         raise RuntimeError("Failed API request.")
     else:
