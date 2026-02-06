565761501faa4ed36ecfbcd428650df471950dfe
Speetr <peetrsullivan@gmail.com>
2026-02-05 18:13:29 -0800

    CH3:L4: Calling Functions

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from google import genai
 from google.genai import types
 import argparse
 from prompts import system_prompt
-from functions.call_function import available_functions
+from functions.call_function import available_functions, call_function
 from functions.get_files_info import get_files_info, schema_get_files_info
 from functions.get_file_content import get_file_content, schema_get_file_content
 from functions.run_python_file import run_python_file, schema_run_python_file
@@ -43,7 +43,21 @@ def main():
 
     if gemini_content.function_calls:
         for function_call in gemini_content.function_calls:
-            print(f"Calling function: {function_call.name}({function_call.args})")
+            #print(f"Calling function: {function_call.name}({function_call.args})")
+            function_call_result = call_function(function_call, args.verbose)
+            if len(function_call_result.parts) == 0:
+                raise Exception("Empty parts list")
+            if not function_call_result.parts[0].function_response:
+                raise Exception("Missing function response")
+            if not function_call_result.parts[0].function_response.response:
+                raise Exception("Missing response")
+            
+            #We have a valid response!
+            function_results = []
+            function_results.append(function_call_result.parts[0])
+
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
     else:
         print(gemini_content.text)
 
