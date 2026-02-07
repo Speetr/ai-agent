d2df4d6f71de0533633e16fddeac38aad7258c37
Speetr <peetrsullivan@gmail.com>
2026-02-06 20:26:05 -0800

    CH4:L1: Agent Loop

--- main.py
+++ main.py
@@ -24,42 +24,53 @@ def main():
 
     client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    gemini_content = client.models.generate_content(
-        model="gemini-2.5-flash", 
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt
+
+    for _ in range(20):
+        gemini_content = client.models.generate_content(
+            model="gemini-2.5-flash", 
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt
+                )
             )
-        )
 
-    if gemini_content.usage_metadata == None:
-        raise RuntimeError("Failed API request.")
-    else:
-        if args.verbose:
-            print(f"User prompt: {args.user_prompt}")
-            print(f"Prompt tokens: {gemini_content.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {gemini_content.usage_metadata.candidates_token_count}")
-
-    if gemini_content.function_calls:
-        for function_call in gemini_content.function_calls:
-            #print(f"Calling function: {function_call.name}({function_call.args})")
-            function_call_result = call_function(function_call, args.verbose)
-            if len(function_call_result.parts) == 0:
-                raise Exception("Empty parts list")
-            if not function_call_result.parts[0].function_response:
-                raise Exception("Missing function response")
-            if not function_call_result.parts[0].function_response.response:
-                raise Exception("Missing response")
-            
-            #We have a valid response!
-            function_results = []
-            function_results.append(function_call_result.parts[0])
-
+        if gemini_content.usage_metadata == None:
+            raise RuntimeError("Failed API request.")
+        else:
             if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-    else:
-        print(gemini_content.text)
+                print(f"User prompt: {args.user_prompt}")
+                print(f"Prompt tokens: {gemini_content.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {gemini_content.usage_metadata.candidates_token_count}")
+
+        if len(gemini_content.candidates) != 0:
+            for candidate in gemini_content.candidates:
+                messages.append(candidate.content)
+    
+        if gemini_content.function_calls:
+            for function_call in gemini_content.function_calls:
+                #print(f"Calling function: {function_call.name}({function_call.args})")
+                function_call_result = call_function(function_call, args.verbose)
+                if len(function_call_result.parts) == 0:
+                    raise Exception("Empty parts list")
+                if not function_call_result.parts[0].function_response:
+                    raise Exception("Missing function response")
+                if not function_call_result.parts[0].function_response.response:
+                    raise Exception("Missing response")
+                
+                #We have a valid response!
+                function_results = []
+                function_results.append(function_call_result.parts[0])
+                messages.append(types.Content(role="user", parts=function_results))
+
+                if args.verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+        else:
+            print(gemini_content.text)
+            return
+        
+    print("Maximum number of loops reached.")
+    exit(1)
 
 
 if __name__ == "__main__":
